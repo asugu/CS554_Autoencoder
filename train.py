59302460147a86b4319b242c53311d3cce01ed32
yilmazdoga <yilmaz.doga@gmail.com>
2023-06-01 17:28:50 +0300

    Logging added

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
-
+import os
 import yaml
 import torch
+import logging
 
 import torch.nn as nn
 import numpy as np
@@ -10,33 +11,44 @@ from networks import *
 from utils import plot_ae_outputs, plot_loss
 
 
-# Specify the path to your YAML file
 config_file = 'config.yaml'
 
-# Read the configuration from the YAML file
+
 with open(config_file, 'r') as file:
     config = yaml.safe_load(file)
+
+save_location = config['save_location']
+if not os.path.exists("results"):
+    os.makedirs("results")
+
+if not os.path.exists("results/" + save_location):
+    os.makedirs("results/" +save_location)
+    
+logging.basicConfig(filename = "results/" +save_location+'/log.log',
+                    filemode = 'w',
+                    level = logging.DEBUG,
+                    format=""
+                    )
+logging.getLogger('matplotlib.font_manager').disabled = True
 
 if config['dataset'] == 'MNISTDataset':
     ds = MNISTDataset(root = './mnist')
     train = ds.train
     test = ds.test
-    print("Selected Dataset: MNIST")
+    logging.info("Selected Dataset: MNIST")
 elif config['dataset'] == 'CIFAR10Dataset':
     ds = CIFAR10Dataset(root = './cifar10')
     train = ds.train
     test = ds.test
-    print("Selected Dataset: CIFAR10")
-
+    logging.info("Selected Dataset: CIFAR10")
     
 batch_size = config['batch_size']
 lr = config['lr']
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps")
 train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=True)
-print(f"Selected device: {device}")
-print(f"Batch Size: {batch_size}")
-print()
+logging.info(f"Selected device: {device}")
+logging.info(f"Batch Size: {batch_size}")
 
 # flatten pre_process
 flatten_size = config['flatten_size']
@@ -48,27 +60,25 @@ if config['network'] == 'BaseCAE':
     model = BaseCAE(color_channel=config['color_channel'], flatten_size=eval(flatten_size), hidden_dim=config['hidden_dim'],
                       latent_dim=config['latent_dim'], tupled_flatten=tupled_flatten)
     model.to(device)
-    print("Selected network: BaseCAE")
-    print(f"Network flatten_size: {tupled_flatten} \nLatent Dim: {config['latent_dim']} \nHidden Dim: {config['hidden_dim']}")
-    print()
+    logging.info("Selected network: BaseCAE")
+    logging.info(f"Network flatten_size: {tupled_flatten} \nLatent Dim: {config['latent_dim']} \nHidden Dim: {config['hidden_dim']}")
 ## Add elif statements for additional networks
 
 
 if config['loss'] == 'MSE':
     criterion = nn.MSELoss()
-    print('Selected loss function: MSE')
-    
+    logging.info('Selected loss function: MSE')
 ### Add elif statements for additional losses
 
 if config['optimizer'] == 'Adam':
     optimizer = torch.optim.Adam(params=model.parameters(),lr=lr, weight_decay=1e-5)
-    print("Selected Optimizer: Adam")
-    print(f"Learning Rate: {lr}")
+    logging.info("Selected Optimizer: Adam")
+    logging.info(f"Learning Rate: {lr}")
 
 ## Add elif statements for additional optimizers
 
 EPOCH = config['epoch']
-
+print("Training Started...")
 mean_train_loss, validation_loss = [], []
 for epoch in range(EPOCH):
     train_loss = []
@@ -97,9 +107,12 @@ for epoch in range(EPOCH):
         preds, ys = torch.cat(preds), torch.cat(ys)
         val_loss = criterion(preds, ys)
         validation_loss.append(val_loss.data)
-
-    print(
-        f"Epoch: {epoch}, Train Loss: {mean_train_loss[epoch]:.4f}, Val Loss: {validation_loss[epoch]:.4f}"
+        
+    logging.info(
+         f"Epoch: {epoch}, Train Loss: {mean_train_loss[epoch]:.4f}, Val Loss: {validation_loss[epoch]:.4f}"
     )
-plot_ae_outputs(model, test, "./mnist_saved")
-plot_loss(mean_train_loss, validation_loss, np.linspace(0, EPOCH, EPOCH), './mnist_saved')
+avg_psnr, avg_ssim = plot_ae_outputs(model, test, "./results/" + save_location)
+logging.info(f"Average PSNR: {avg_psnr:.2f}")
+logging.info(f"Average SSIM: {avg_ssim:.4f}")
+plot_loss(mean_train_loss, validation_loss, np.linspace(0, EPOCH, EPOCH), "./results/" + save_location)
+print("Training Ended...")
